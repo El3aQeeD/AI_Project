488fd6ef15684642b8ae6b40ed9854ca242f1b38
Khaled Mostafa <95879454+El3aQeeD@users.noreply.github.com>
2022-10-28 17:03:17 +0200

    hello

--- GUI_Code.py
+++ GUI_Code.py
@@ -78,11 +78,4 @@ while True:
             else:
                 pygame.draw.rect(surface, wall_color, pygame.Rect(x[i].x, x[i].y, x[i].wd - 2, x[i].ht - 2))
 
-#khalid
-    #hos
-
     pygame.display.flip()
-
-#helllooo
-#hiii
-#hany
