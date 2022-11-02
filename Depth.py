0e06a9a1814605ebe8df8fe555f73993f774ecff
hany170 <100557057+hany170@users.noreply.github.com>
2022-11-03 01:25:27 +0200

    clean the code and add the path

--- Depth.py
+++ Depth.py
@@ -5,7 +5,9 @@ class CActor:
     wd=70 #width of the square
     type=0 # type 0 for normal square - 1 for start and end squares - 2 for walls that stops the search
     row=-1
-    coloum=-1
+    column=-1
+    BNode_r=-1#the row of the Back Node
+    BNode_c = -1#the column of the Back Node
 
 # Importing the library
 import pygame
@@ -13,14 +15,9 @@ import sys
 clock = pygame.time.Clock()
 def visited_Node_check(node):
     global visited
-
     for x in visited:
         if node == x:
-            print("viv")
             return True
-
-
-
     return False
 def drawing_Game():
     # Drawing Squares
@@ -32,69 +29,73 @@ def drawing_Game():
                 pygame.draw.rect(surface, startAndend_color, pygame.Rect(x[i].x, x[i].y, x[i].wd - 2, x[i].ht - 2))
             elif x[i].type == 2:
                 pygame.draw.rect(surface, wall_color, pygame.Rect(x[i].x, x[i].y, x[i].wd - 2, x[i].ht - 2))
-            else:
+            elif x[i].type == 3:
                 pygame.draw.rect(surface, visited_node_color, pygame.Rect(x[i].x, x[i].y, x[i].wd - 2, x[i].ht - 2))
+            else: pygame.draw.rect(surface, path_color, pygame.Rect(x[i].x, x[i].y, x[i].wd - 2, x[i].ht - 2))
 
     pygame.display.flip()
-def fringe_Check():
-    global fringe, ctBreak, ctBreak2
-    ctBreak = len(fringe);
-    mylist = fringe.copy()
-    for x in mylist:
-        if visited_Node_check(x) == False:
-            if x == largeList[end_r][end_c]:
-                print("found it")
-                return True
-            else:
-                expand_Node(x)
-                print("3alih eldor")
-                print(x.row)
-                print(x.coloum)
-                fringe.pop(0)
-    ctBreak2 = len(fringe)
-    if ctBreak == ctBreak2:
-        return True
 def Depth_Serching():
     global fringe
     while True:
-        current_node=fringe.pop()
-        print(current_node.row , ",",current_node.coloum)
-        if(current_node==largeList[end_r][end_c]):
+        if len(fringe)!=0:
+            current_node=fringe.pop()#pop the last pushed node and save it in current_node
+            print(current_node.row, ",", current_node.column)
+            if(current_node==largeList[end_r][end_c]):#check if the current_node(poped node)is the end or not
+                print("Last")
+                print(current_node.row, ",", current_node.column)
+                print("len",len(visited))
+                print("found it")
+                while True:
+                    largeList[current_node.BNode_r][current_node.BNode_c].type=4 #chagne the color of the path
 
-            print("Last")
-            print(current_node.row , ",",current_node.coloum)
-            print("len",len(visited))
-            print("found it")
-            return True
-            break
-        if visited_Node_check(current_node)==True:
-            continue
-        expand_Node(current_node)
-        visited.append(largeList[current_node.row][current_node.coloum])
-        largeList[current_node.row][current_node.coloum].type=3
-        drawing_Game()
-        clock.tick(1)
+                    current_node=largeList[current_node.BNode_r][current_node.BNode_c]
+                    if(current_node==largeList[start_r][start_c]):
+                        print("opaa")
+                        break
+                return True
+                break
+            if visited_Node_check(current_node)==True:#check if the current_node(poped node) visited or not
+                continue#if it in the visited list
+            if expand_Node(current_node)==False:
+                return True
 
-
-
-
-
-
-
+            visited.append(largeList[current_node.row][current_node.column])#add the current_node(poped node) to the visited list
+            largeList[current_node.row][current_node.column].type=3
+            drawing_Game()
+            clock.tick(2)
+        else:return True
 def expand_Node(node):
     global fringe
-    if(node.coloum>0):
-        if (visited_Node_check(largeList[node.row][node.coloum-1]) == False):
-            fringe.append(largeList[node.row][node.coloum-1])
+    #push left,Down,Right and Up in the order
+    Flag=0#Flag to check if a node is surrounded by a walls
+    if(node.column>0):
+        if (visited_Node_check(largeList[node.row][node.column - 1]) == False):
+            largeList[node.row][node.column - 1].BNode_r=node.row
+            largeList[node.row][node.column - 1].BNode_c = node.column
+            fringe.append(largeList[node.row][node.column - 1])
+            Flag = 1
+
     if(node.row<7):
-        if (visited_Node_check(largeList[node.row+1][node.coloum]) == False):
-            fringe.append(largeList[node.row+1][node.coloum])
-    if(node.coloum<7):
-        if (visited_Node_check(largeList[node.row][node.coloum + 1]) == False):
-            fringe.append(largeList[node.row][node.coloum+1])
+        if (visited_Node_check(largeList[node.row+1][node.column]) == False):
+            largeList[node.row + 1][node.column].BNode_r=node.row
+            largeList[node.row + 1][node.column].BNode_c = node.column
+            fringe.append(largeList[node.row+1][node.column])
+            Flag = 2
+    if(node.column<7):
+        if (visited_Node_check(largeList[node.row][node.column + 1]) == False):
+            largeList[node.row][node.column + 1].BNode_r=node.row
+            largeList[node.row][node.column + 1].BNode_c = node.column
+            fringe.append(largeList[node.row][node.column + 1])
+            Flag = 3
     if(node.row>0):
-        if (visited_Node_check(largeList[node.row-1][node.coloum]) == False):
-            fringe.append(largeList[node.row-1][node.coloum])
+        if (visited_Node_check(largeList[node.row-1][node.column]) == False):
+            largeList[node.row - 1][node.column].BNode_r=node.row
+            largeList[node.row - 1][node.column].BNode_c = node.column
+            fringe.append(largeList[node.row-1][node.column])
+            Flag = 4
+    if(Flag==0 & largeList[node.row][node.column - 1].type==2 & largeList[node.row+1][node.column].type==2 & largeList[node.row][node.column + 1].type==2 & largeList[node.row-1][node.column].type==2):#if the flag is surrounded by a walls it will return false
+        return False
+    else:return True
 
 n=8
 thisList=[]
@@ -107,7 +108,7 @@ for r in range(n):
         act.x= act.x + ( c * act.wd)
         act.y= act.y + ( r * act.ht)
         act.row = r
-        act.coloum = c
+        act.column = c
 
         thisList.append(act)
 
@@ -127,6 +128,7 @@ normal_color = (100, 88, 99)
 startAndend_color = (255,0,0)
 wall_color=(100,50,0)
 visited_node_color=(0, 255, 0)
+path_color=(255,215,0)
 #custom varibale
 ctBreak=0
 ctBreak2=0
@@ -169,8 +171,10 @@ while True:
                                     start_c = c
                                     fringe.append(largeList[r][c])
                                 if ctClicks == 2:
-                                    end_r = r
-                                    end_c = c
+                                    if largeList[r][c]!=largeList[start_r][start_c]:
+                                        end_r = r
+                                        end_c = c
+                                    else:ctClicks-=1
 
         # check if user click any button and we use it to put walls
         if event.type == pygame.KEYDOWN:
